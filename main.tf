d060619ab589a4d76e435b1ef0b642132ddcd5ee
Raghu K <raghuk.vit@gmail.com>
2022-06-09 08:35:27 +0530

    Update roboshop-infra/payment-infra/

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "vpc" {
 
 module "rabbitmq" {
   source     = "git::https://github.com/raghudevopsb64/tf-module-rabbitmq.git"
-  COMPONENT  = var.COMPONENT
+  COMPONENT  = "rabbitmq"
   ENV        = var.ENV
   VPC_ID     = module.vpc.VPC_ID
   VPC_CIDR   = module.vpc.VPC_CIDR
