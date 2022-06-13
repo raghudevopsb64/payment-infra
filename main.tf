64d805893dada393f16eed4fd8a973541fcd3f70
Raghu K <raghuk.vit@gmail.com>
2022-06-13 08:28:13 +0530

    Update roboshop-infra/payment-infra/

--- main.tf
+++ main.tf
@@ -18,19 +18,19 @@ module "rabbitmq" {
   WORKSTATION_IP = var.WORKSTATION_IP
 }
 
-module "app" {
-  depends_on              = [module.vpc]
-  source                  = "git::https://github.com/raghudevopsb64/tf-modfule-mutable.git"
-  ONDEMAND_INSTANCE_COUNT = var.ONDEMAND_INSTANCE_COUNT
-  SPOT_INSTANCE_COUNT     = var.SPOT_INSTANCE_COUNT
-  VPC_ID                  = module.vpc.VPC_ID
-  VPC_CIDR                = module.vpc.VPC_CIDR
-  SUBNET_IDS              = module.vpc.SUBNET_IDS
-  COMPONENT               = var.COMPONENT
-  ENV                     = var.ENV
-  NODE_TYPE               = var.EC2_NODE_TYPE
-  PORT                    = var.PORT
-  WORKSTATION_IP          = var.WORKSTATION_IP
-  IAM_POLICY_CREATE       = true
-  VPC_ACCESS_TO_ALB       = [module.vpc.VPC_CIDR, "10.10.5.0/24"]
-}
+//module "app" {
+//  depends_on              = [module.vpc]
+//  source                  = "git::https://github.com/raghudevopsb64/tf-modfule-mutable.git"
+//  ONDEMAND_INSTANCE_COUNT = var.ONDEMAND_INSTANCE_COUNT
+//  SPOT_INSTANCE_COUNT     = var.SPOT_INSTANCE_COUNT
+//  VPC_ID                  = module.vpc.VPC_ID
+//  VPC_CIDR                = module.vpc.VPC_CIDR
+//  SUBNET_IDS              = module.vpc.SUBNET_IDS
+//  COMPONENT               = var.COMPONENT
+//  ENV                     = var.ENV
+//  NODE_TYPE               = var.EC2_NODE_TYPE
+//  PORT                    = var.PORT
+//  WORKSTATION_IP          = var.WORKSTATION_IP
+//  IAM_POLICY_CREATE       = true
+//  VPC_ACCESS_TO_ALB       = [module.vpc.VPC_CIDR, "10.10.5.0/24"]
+//}
