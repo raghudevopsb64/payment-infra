9b6d2513ef21b25652330a606c4f78dd722198f3
Raghu K <raghuk.vit@gmail.com>
2022-06-15 09:19:07 +0530

    Update roboshop-infra/payment-infra/

--- main.tf
+++ main.tf
@@ -33,4 +33,5 @@ module "app" {
   WORKSTATION_IP          = var.WORKSTATION_IP
   IAM_POLICY_CREATE       = true
   VPC_ACCESS_TO_ALB       = [module.vpc.VPC_CIDR, "10.10.5.0/24"]
+  PRIVATE_HOSTED_ZONE_ID  = module.vpc.PRIVATE_HOSTED_ZONE_ID
 }
